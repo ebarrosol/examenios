6fb55ea68a036dd0d8367506661326d3b33a52c2
Efren Alejandro Barroso Llanes <efrenalejandrobll@Efrens-Mac.local>
2015-06-13 08:51:35 -0500

    Initial Commit

---
+++ BlockNotas/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BlockNotas
+//
+//  Created by Efren Alejandro Barroso Llanes on 13/06/15.
+//  Copyright (c) 2015 EBL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
